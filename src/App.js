e7da01a21b37450f38068d3cd32b5cde89ba6c5d
Phucndh <nucbeo1610@gmail.com>
2021-11-21 22:40:27 +0700

    update listmovie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import './App.css';
+import './assets/style/main.scss';
 import { createBrowserHistory } from 'history';
 import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
 import { Router, Switch } from 'react-router';
 import Home from './pages/Home/Home';
+import ListMovieComponents from './Components/ListMovieComponents/ListMovieComponents'
 export const history = createBrowserHistory();
-
-
 function App() {
-
   return (
-
     <Router history={history}>
       <Switch>
         <HomeTemplate path="/" exact Component={Home} />
+        <HomeTemplate path="/listmovie" exact Component={ListMovieComponents} />
       </Switch>
     </Router>
-
-
-    
   );
 }
 
--- src/Components/ListMovieComponents/ListMovieComponents.js
+++ src/Components/ListMovieComponents/ListMovieComponents.js
@@ -1,11 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Movie from '../Movie/Movie';
+import ListMovieCarousel from './ListMovieCarousel/ListMovieCarousel';
 
-export default class ListMovieComponents extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+export default function ListMovieComponents() {
+    return (
+        <div id="lichChieu">
+            <section className="listMovie container">
+                <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link active button"
+                            id="pills-dangChieu-tab"
+                            data-toggle="pill"
+                            href="#pills-dangChieu"
+                            role="tab"
+                            aria-controls="pills-dangChieu"
+                            aria-selected="true">
+                            Đang Chiếu
+                        </button>
+                    </li>
+                    <li className="nav-item" role="presentation">
+                        <button className="nav-link button"
+                            id="pills-sapChieu-tab"
+                            data-toggle="pill"
+                            href="#pills-sapChieu"
+                            role="tab"
+                            aria-controls="pills-sapChieu"
+                            aria-selected="false">
+                            Sắp Chiếu
+                        </button>
+                    </li>
+                </ul>
+                <div className="tab-content listMovie__content" id="pills-tabContent">
+                    <div
+                        className="tab-pane fade show active content__item "
+                        id="pills-dangChieu"
+                        role="tabpanel"
+                        aria-labelledby="pills-dangChieu-tab"
+                    >
+                        <div className="content__dangChieu">
+                            <ListMovieCarousel/>
+                        </div>
+                    </div>
+                    <div
+                        className="tab-pane fade content__item"
+                        id="pills-sapChieu"
+                        role="tabpanel"
+                        aria-labelledby="pills-sapChieu-tab"
+                    >
+                        <div className="content__dangChieu">
+                            <ListMovieCarousel/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
 }
