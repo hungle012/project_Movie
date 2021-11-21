ba65a50b42409c435c0645f187490775f6581c84
Hung <kieuhungle12@gmail.com>
2021-11-21 08:51:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
-import './assets/style/main.scss';
 import { createBrowserHistory } from 'history';
 import { HomeTemplate } from './templates/HomeTemplate/HomeTemplate';
 import { Router, Switch } from 'react-router';
 import Home from './pages/Home/Home';
 export const history = createBrowserHistory();
+
+
 function App() {
 
   return (
+
     <Router history={history}>
       <Switch>
         <HomeTemplate path="/" exact Component={Home} />
       </Switch>
     </Router>
+
+
+    
   );
 }
 
