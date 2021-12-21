6fd609db79617be2720ec47e1b8c82d81aeb0d23
Hung <kieuhungle12@gmail.com>
2021-12-21 15:02:07 +0700

    fisrt cm

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -7,10 +7,11 @@ export default function Loading(props) {
 
     return (
         <Fragment>
-            { isLoading ?
+            {isLoading ?
                 <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,0.4)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 99 }}>
-                    <div className='display-4 text-light'> Loading...</div>
-                </div> : ''  
+                    <div className='display-4 text-light'>...Loading
+                    </div>
+                </div> : ''
             }
         </Fragment>
 
--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,8 +1,9 @@
 import Axios from "axios";
-import { CYBER_TOKEN, DOMAIN ,TOKEN} from "../util/settings/config";
+import { CYBER_TOKEN, DOMAIN, TOKEN } from "../util/settings/config";
 
 export class baseServices {
     //put json về phía backend
+<<<<<<< HEAD
     put = (url,model) => {
         return  Axios({
             url:`${DOMAIN}${url}`,
@@ -10,31 +11,72 @@ export class baseServices {
             data:model,
             headers: {'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN)),'TokenCybersoft': CYBER_TOKEN} //JWT
         }) 
+=======
+    put = (url, model) => {
+        return Axios({
+            url: `${DOMAIN}${url}`,
+            method: 'PUT',
+            data: model,
+            headers: {
+                'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN))
+                , 'TokenCybersoft': CYBER_TOKEN
+            } //JWT
+        })
+>>>>>>> 1678c189f9f5d4ce9fb1e34ebcaea4ab70dc0d6a
     }
 
-    post = (url,model) => {
+    post = (url, model) => {
         return Axios({
+<<<<<<< HEAD
             url:`${DOMAIN}${url}`,
             method:'POST',
             data:model,
             headers: {'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN)),'TokenCybersoft': CYBER_TOKEN} //JWT
         }) 
+=======
+            url: `${DOMAIN}${url}`,
+            method: 'POST',
+            data: model,
+            headers: {
+                'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN))
+                , 'TokenCybersoft': CYBER_TOKEN
+            } //JWT
+        })
+>>>>>>> 1678c189f9f5d4ce9fb1e34ebcaea4ab70dc0d6a
     }
 
 
     get = (url) => {
         return Axios({
+<<<<<<< HEAD
             url:`${DOMAIN}${url}`,
             method:'GET',
             headers: {'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN)),'TokenCybersoft': CYBER_TOKEN} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+=======
+            url: `${DOMAIN}${url}`,
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN))
+                , 'TokenCybersoft': CYBER_TOKEN
+            } //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+>>>>>>> 1678c189f9f5d4ce9fb1e34ebcaea4ab70dc0d6a
         })
     }
 
     delete = (url) => {
         return Axios({
+<<<<<<< HEAD
             url:`${DOMAIN}${url}`,
             method:'DELETE',
             headers: {'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN)),'TokenCybersoft': CYBER_TOKEN} //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+=======
+            url: `${DOMAIN}${url}`,
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + (localStorage.getItem(TOKEN))
+                , 'TokenCybersoft': CYBER_TOKEN
+            } //token yêu cầu từ backend chứng minh user đã đăng nhập rồi
+>>>>>>> 1678c189f9f5d4ce9fb1e34ebcaea4ab70dc0d6a
         })
     }
 }
