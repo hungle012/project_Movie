e9756d3a0291eeec69932f64e9432e81fa635c66
hungle <hung.le@madison-technology.com>
2023-03-22 23:23:01 +0700

    renew token

--- src/util/settings/config.js
+++ src/util/settings/config.js
@@ -1,7 +1,8 @@
 // định nghĩa các tham số cố định
-export const DOMAIN = 'https://movienew.cybersoft.edu.vn';
-export const TOKEN = 'accessToken';
-export const GROUPID = 'GP04';
-export const GROUPIDSC = 'GP03';
-export const USER_LOGIN = 'USER_LOGIN';
-export const CYBER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwMSIsIkhldEhhblN0cmluZyI6IjMwLzA2LzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY1NjU0NzIwMDAwMCIsIm5iZiI6MTYyMDkyNTIwMCwiZXhwIjoxNjU2Njk0ODAwfQ.6o2C_IS8e7HlB9dUZ9eFRYOb2ST9LjIIbn4fO_SS1Qc';
+export const DOMAIN = "https://movienew.cybersoft.edu.vn";
+export const TOKEN = "accessToken";
+export const GROUPID = "GP04";
+export const GROUPIDSC = "GP03";
+export const USER_LOGIN = "USER_LOGIN";
+export const CYBER_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAzNSIsIkhldEhhblN0cmluZyI6IjAzLzA2LzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY4NTc1MDQwMDAwMCIsIm5iZiI6MTY1NzczMTYwMCwiZXhwIjoxNjg1ODk4MDAwfQ.KXn1XtehbphvfW3OSUFlLIzSrEtSLDtDQG4BgF38Cus";
